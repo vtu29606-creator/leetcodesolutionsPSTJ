71746219d971e057ce783234e01a49d72690e649
vtu29606-creator <vtu29606@veltech.edu.in>
2026-09-10 11:36:17 +0530

    Delete Sort Array By Parity.java

--- Sort Array By Parity.java
+++
@@ -1,9 +0,0 @@
-    public int[] sortArrayByParity(int[] A) {
-        for (int i = 0, j = 0; j < A.length; j++)
-            if (A[j] % 2 == 0) {
-                int tmp = A[i];
-                A[i++] = A[j];
-                A[j] = tmp;;
-            }
-        return A;
-    }
