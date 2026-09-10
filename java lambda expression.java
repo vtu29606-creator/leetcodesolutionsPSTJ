7c8e43bcb39a6801e767e726cbeed5be2d346abe
vtu29606-creator <vtu29606@veltech.edu.in>
2026-09-10 10:30:39 +0530

    Implement lambda expressions for number checks

--- java lambda expression.java
+++ java lambda expression.java
@@ -1,1 +1,45 @@
+import java.util.*;
+
+interface PerformOperation {
+    boolean check(int a);
+}
+
+class MyMath {
+
+    public boolean checker(PerformOperation p, int num) {
+        return p.check(num);
+    }
+
+    public PerformOperation isOdd() {
+        return n -> n % 2 != 0;
+    }
+
+    public PerformOperation isPrime() {
+        return n -> {
+            if (n < 2) return false;
+
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    }
+
+    public PerformOperation isPalindrome() {
+        return n -> {
+            int original = n;
+            int reverse = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                reverse = reverse * 10 + digit;
+                n /= 10;
+            }
+
+            return original == reverse;
+        };
+    }
+}
 
